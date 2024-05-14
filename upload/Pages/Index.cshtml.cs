e9430e69a10eabf1973eef76b6be9e32d9b44fbf
František Šilhán <frantisek@slhn.cz>
2024-05-14 10:15:53 +0200

    add thumbnails

--- upload/Pages/Index.cshtml.cs
+++ upload/Pages/Index.cshtml.cs
@@ -13,7 +13,7 @@ namespace upload.Pages {
         public string SuccessMessage { get; set; }
         [TempData]
         public string ErrorMessage { get; set; }
-        public List<StoredFile> Files { get; set; } = new List<StoredFile>();
+        public List<StoredFileListViewModel> Files { get; set; } = new List<StoredFileListViewModel>();
 
         public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context) {
             _logger = logger;
@@ -21,7 +21,19 @@ namespace upload.Pages {
         }
 
         public async Task<IActionResult> OnGet() {
-            Files = await _context.StoredFiles.ToListAsync();
+            Files = await _context.StoredFiles.AsNoTracking()
+                .Include(f => f.Author)
+                .Include(f => f.Thumbnails)
+                .Select(f => new StoredFileListViewModel {
+                    StoredFileId = f.StoredFileId,
+                    MimeType = f.MimeType,
+                    OriginalName = f.OriginalName,
+                    AuthorId = f.AuthorId,
+                    Author = f.Author,
+                    CreatedAt = f.CreatedAt,
+                    ThumbnailCount = f.Thumbnails == null ? 0 : f.Thumbnails.Count
+                })
+                .ToListAsync();
             return Page();
         }
 
@@ -33,6 +45,32 @@ namespace upload.Pages {
                 ErrorMessage = "There is no such file.";
                 return RedirectToPage();
             }
+        }
+        public async Task<IActionResult> OnGetThumbnail(string filename, ThumbnailType type = ThumbnailType.Square) {
+            StoredFile file = await _context.StoredFiles
+              .AsNoTracking()
+              .Where(f => f.StoredFileId == Guid.Parse(filename))
+              .SingleOrDefaultAsync();
+            if (file == null) {
+                return NotFound("no record for this file");
+            }
+            Thumbnail thumbnail = await _context.Thumbnails
+              .AsNoTracking()
+              .Where(t => t.StoredFileId == Guid.Parse(filename) && t.Type == type)
+              .SingleOrDefaultAsync();
+            if (thumbnail != null) {
+                return File(thumbnail.Blob, file.MimeType);
+            }
+            return NotFound("no thumbnail for this file");
+        }
+        public class StoredFileListViewModel {
+            public Guid StoredFileId { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public Guid AuthorId { get; set; }
+            public User Author { get; set; }
+            public string OriginalName { get; set; }
+            public string MimeType { get; set; }
+            public int ThumbnailCount { get; set; }
         }
     }
 }
--- upload/Pages/Upload.cshtml.cs
+++ upload/Pages/Upload.cshtml.cs
@@ -1,5 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Processing;
 using System.Security.Claims;
 using upload.Data;
 using upload.Models;
@@ -7,6 +10,9 @@ using upload.Models;
 namespace upload.Pages {
     public class UploadModel : PageModel {
         private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly int _squareSize;
+        private readonly int _sameAspectRatioHeigth;
 
         [TempData]
         public string SuccessMessage { get; set; }
@@ -15,8 +21,11 @@ namespace upload.Pages {
 
         public ICollection<IFormFile> Upload { get; set; }
 
-        public UploadModel(ApplicationDbContext context) {
+        public UploadModel(ApplicationDbContext context, IConfiguration configuration) {
             _context = context;
+            _configuration = configuration;
+            if (Int32.TryParse(_configuration["Thumbnails:SquareSize"], out _squareSize) == false) _squareSize = 64; // získej data z konfigurave nebo použij 64
+            if (Int32.TryParse(_configuration["Thumbnails:SameAspectRatioHeigth"], out _sameAspectRatioHeigth) == false) _sameAspectRatioHeigth = 128;
         }
 
         public void OnGet() {
@@ -34,6 +43,38 @@ namespace upload.Pages {
                         AuthorId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!),
                         CreatedAt = DateTime.Now
                     };
+
+                    if (uploadedFile.ContentType.StartsWith("image")) // je soubor obrázek?
+                    {
+                        file.Thumbnails = new List<Thumbnail>();
+                        MemoryStream ims = new MemoryStream(); // proud pro příchozí obrázek
+                        MemoryStream oms1 = new MemoryStream(); // proud pro čtvercový náhled
+                        MemoryStream oms2 = new MemoryStream(); // proud pro obdélníkový náhled
+                        uploadedFile.CopyTo(ims); // vlož obsah do vstupního proudu
+                        IImageFormat format; // zde si uložíme formát obrázku (JPEG, GIF, ...), budeme ho potřebovat při ukládání
+                        using (Image image = Image.Load(ims.ToArray())) // vytvoříme čtvercový náhled
+                        {
+                            format = image.Configuration.ImageFormats.First();
+                            int largestSize = Math.Max(image.Height, image.Width); // jaká je orientace obrázku?
+                            if (image.Width > image.Height) // podle orientace změníme velikost obrázku
+                            {
+                                image.Mutate(x => x.Resize(0, _squareSize));
+                            } else {
+                                image.Mutate(x => x.Resize(_squareSize, 0));
+                            }
+                            image.Mutate(x => x.Crop(new Rectangle((image.Width - _squareSize) / 2, (image.Height - _squareSize) / 2, _squareSize, _squareSize)));
+                            // obrázek ořízneme na čtverec
+                            image.Save(oms1, format); // vložíme ho do výstupního proudu
+                            file.Thumbnails.Add(new Thumbnail { Type = ThumbnailType.Square, Blob = oms1.ToArray() }); // a uložíme do databáze jako pole bytů
+                        }
+                        using (Image image = Image.Load(ims.ToArray())) // obdélníkový náhled začíná zde
+                        {
+                            format = image.Configuration.ImageFormats.First();
+                            image.Mutate(x => x.Resize(0, _sameAspectRatioHeigth)); // stačí jen změnit jeho velikost
+                            image.Save(oms2, format); // a přes proud ho uložit do databáze
+                            file.Thumbnails.Add(new Thumbnail { Type = ThumbnailType.SameAspectRatio, Blob = oms2.ToArray() });
+                        }
+                    }
 
                     using (var memoryStream = new MemoryStream()) {
                         await uploadedFile.CopyToAsync(memoryStream);
